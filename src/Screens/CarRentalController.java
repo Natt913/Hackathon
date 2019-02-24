0db194c523cdc2e863064aad47eef0d565e1dae7
Natt913 <nathalie.crespo913@gmail.com>
2019-02-24 06:15:02 -0500

    Screens!

--- src/Screens/CarRentalController.java
+++ src/Screens/CarRentalController.java
@@ -1,4 +1,18 @@
 package Screens;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
 public class CarRentalController {
+
+    @FXML
+    ImageView lock;
+
+    @FXML
+    ImageView unlock;
+
+    @FXML
+    ImageView flashLights;
+
+
 }
